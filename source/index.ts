5ff448fe2cfea341f4f1a43322ddd51c5ebe2990
LaukikOnChromeBook <22937426+LRagji@users.noreply.github.com>
2022-05-07 16:20:20 +0530

    Added documentation for IRedisClient interface

--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,40 @@
 import path from "path";
 /**
-* Interface used to abstract a redis client to the package
+* Interface used to abstract a redis client/connection for this package
 */
 export interface IRedisClient {
+    /**
+     * This method acquires a redis client instance from a pool of redis clients with token as an identifier/handle.
+     * @param token A unique string used to acquire a redis client instance against. Treat this as redis client handle.
+     */
     acquire(token?: string): Promise<void>
+    /**
+     * This method releases the acquired redis client back into the pool.
+     * @param token A unique string used when acquiring client via {@link acquire} method
+     */
     release(token?: string): Promise<void>
+    /**
+     * Signals a dispose method to the pool stating no more clients will be needed, donot call any methods post calling shutdown. 
+     */
     shutdown(): Promise<void>
+    /**
+     * Executes a single command on acquired connection.
+     * @param commandArgs Array of strings including commands and arguments Eg:["set","key","value"]
+     * @returns Promise of any type.
+     */
     run(commandArgs: string[]): Promise<any>
+    /**
+     * This method is used to execute a set of commands in one go sequentially on redis side.
+     * @param commands Array of array of strings including multiple commands and arguments that needs to be executed in one trip to the server sequentially. Eg:[["set","key","value"],["get","key"]]
+     * @returns Promise of all results in the commands(any type).
+     */
     pipeline(commands: string[][]): Promise<any>;
+    /**
+     * This method is used to execute a lua script on redis connection.
+     * @param filename Full file path of the lua script to be executed Eg: path.join(__dirname, "script.lua")
+     * @param keys Array of strings, Keys to be passsed to the script. 
+     * @param args Array of strings, Arguments to be passed to the script.
+     */
     script(filename: string, keys: string[], args: string[]): Promise<any>
 }
 
